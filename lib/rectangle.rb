5cb6dbbffab8b15c7d1eef82f263ff74a72edb25
Natalie Gregory <gregorynr@gmail.com>
2014-07-30 10:36:00 -0600

    warm-up first commit

---
+++ lib/rectangle.rb
@@ -0,0 +1,9 @@
+class Rectangle
+  def initialize (length, width)
+    @length = length
+    @width = width
+  end
+  def area
+    puts @length * @width
+  end
+end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,17 @@
 require 'spec_helper'
 
+describe Rectangle do
+
+  describe "length" do
+
+  end
+
+  describe "width" do
+
+  end
+
+  describe "area" do
+
+  end
+
+end
